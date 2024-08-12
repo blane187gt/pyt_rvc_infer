ce6cb1d72b7eb6896d46e20fdf7b2b9c6104e6e1
blane187gt <167639579+blane187gt@users.noreply.github.com>
2024-08-13 04:53:53 +0700

    Update infer.py

--- pyt_rvc_infer/infer.py
+++ pyt_rvc_infer/infer.py
@@ -50,26 +50,7 @@ class Configs:
 
         return x_pad, x_query, x_center, x_max
 
-def download_model(url, filename):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(filename, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
-        print(f"Downloaded {filename}")
-    else:
-        print(f"Failed to download {filename}")
 
-def download_models():
-    urls = {
-        "fcpe.pt": "https://huggingface.co/datasets/ylzz1997/rmvpe_pretrain_model/resolve/main/fcpe.pt",
-        "hubert_base.pt": "https://huggingface.co/Kit-Lemonfoot/RVC_DidntAsk/resolve/main/hubert_base.pt",
-        "rmvpe.pt": "https://huggingface.co/Kit-Lemonfoot/RVC_DidntAsk/resolve/main/rmvpe.pt"
-    }
-    
-    for filename, url in urls.items():
-        if not os.path.exists(filename):
-            download_model(url, filename)
 
 def get_model(voice_model):
     model_dir = os.path.join(os.getcwd(), "models", voice_model)
